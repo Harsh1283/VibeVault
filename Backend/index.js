fe227ba014f9fb220ae772f48bfd8ffa3a2cbbb3
Harsh1283 <harshsrivas233@gmail.com>
2025-11-06 01:58:57 +0530

    change some .env

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import connectDB from "./src/db/db.coonect.js";
 import app from "./src/app.js";
-
+import dotenv from "dotenv";
+dotenv.config();
 connectDB();
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,9 +6,13 @@ import cors from "cors";
 
 const app=express();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 
 app.use(express.json());
 app.use(cookieParser());
